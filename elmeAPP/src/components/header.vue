b5589653ed0df64abeecfb273c73b8b4420e585c
张久屹 <447488560@qq.com>
2017-07-31 23:43:00 +0800

    header

--- elmeAPP/src/components/header.vue
+++ elmeAPP/src/components/header.vue
@@ -34,19 +34,41 @@
   	<div class="background">
   		<img :src="seller.avatar" width=100% height=100%>
   	</div>	
-  	<div class="detail" v-show='detailShow'>
-  		<div class="detail-wrapper">
-  			<div class="detail-main">
-  				<h1 class='name'>{{seller.name}}</h1>
-  				<star :size='48' :score='seller.score'></star>
-  	
-  			</div>
-  		</div>
-  		<div class="detail-close">
-  			<i class='icon-close'></i>
-  		</div>
-  	</div>
-
+  	<!-- 遮罩 -->
+  	<transition name='fade'>
+	  	<div class="detail" v-show='detailShow'>
+	  		<div class="detail-wrapper">
+	  			<div class="detail-main">
+	  				<h1 class='name'>{{seller.name}}</h1>
+	  				<div class="star-wrapper">
+	  					<star :size='48' :score='seller.score'></star>
+	  				</div>
+	  				<div class="title">
+	  					<div class="line"></div>
+	  					<div class="text">优惠信息</div>
+	  					<div class="line"></div>
+	  				</div>
+	  				<ul class="supports" v-if='seller.supports'>
+		  				<li class='support-item' v-for='(item,index) in seller.supports'>
+		  					<span class='icon' :class='classMap[seller.supports[index].type]'></span>
+		  					<span class="text">{{seller.supports[index].description}}</span>
+		  				</li>
+		  			</ul>
+						<div class="title">
+	  					<div class="line"></div>
+	  					<div class="text">商家公告</div>
+	  					<div class="line"></div>
+	  				</div>
+	  				<div class="bulletin">
+	  						<p class='content'>{{seller.bulletin}}</p>
+						</div>
+	  			</div>
+	  		</div>
+	  		<div class="detail-close">
+	  			<i class='icon-close' @click='detailShow=!detailShow'></i>
+	  		</div>
+	  	</div>
+  	</transition>
   </div>
 </template>
 
@@ -70,7 +92,7 @@ export default {
     }
   },
   created () {
-
+  	// this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   },
   methods: {
   	showDetail () {
@@ -231,6 +253,7 @@ export default {
 		height: 100%;
 		overflow: auto;
 		background: rgba(7, 17, 27, 0.8);
+		backdrop-filter: blur(10px);
 		.detail-wrapper {
 			box-sizing: border-box;
 			min-height: 100%;
@@ -244,6 +267,76 @@ export default {
 					font-size: 16px;
 					font-weight: 700;
 				}
+				.star-wrapper {
+					margin-top: 18px;
+					padding: 2px 0;
+					text-align: center;
+				}
+				.title {
+					display: flex;
+					width: 80%;
+					margin: 30px auto;
+					.line {
+						flex: 1;
+						position: relative;
+						top: -6px;
+						border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+					}
+					.text {
+						padding: 0 12px; 
+						font-weight: 700;
+						font-size: 14px;
+					}
+				}
+				.supports {
+					width: 80%;
+					margin: 0 auto;
+					.support-item {
+						padding: 0 12px;
+						margin-bottom: 12px;
+						fonte-size: 0;
+						&:last-child {
+							margin-bottom: 0;
+						}
+						.icon {
+							display: inline-block;
+							width: 16px;
+							height: 16px;
+							vertical-align: top;
+							margin-right: 6px;
+							background-size: 16px 16px;
+							background-repeat: no-repeat;
+							&.decrease {
+								background-image: url('./header/decrease_1@2x.png')
+							}
+							&.discount {
+								background-image: url('./header/discount_1@3x.png')
+							}
+							&.special {
+								background-image: url('./header/special_1@3x.png')
+							}
+							&.invoice {
+								background-image: url('./header/invoice_1@3x.png')
+							}
+							&.guarantee {
+								background-image: url('./header/guarantee_1@3x.png')
+							}
+						}
+						.text {
+							line-height: 16px;
+							font-size: 12px;
+						}
+					}
+				}
+				.bulletin {
+					width: 80%;
+					margin: 0 auto;
+					.content {
+						padding: 0 20px;
+						line-height: 24px;
+						font-size: 12px;
+					}
+				}
 			}
 		}
 		.detail-close {
@@ -254,6 +347,13 @@ export default {
 			font-size: 32px
 		}
 	}
+}
+/* 动画过渡 */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
 }
 
 </style>
