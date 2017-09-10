60866482a7a5cdfbf2cdf080d26c10d34e6372da
张久屹 <447488560@qq.com>
2017-09-11 01:21:08 +0800

    transition bug

--- elmeAPP/src/components/shopcart.vue
+++ elmeAPP/src/components/shopcart.vue
@@ -1,62 +1,61 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-    	<div class="content-left">
-	  		<div class="logo-wrapper">
-	  			<div class="logo" :class="{heighlight: totalCount >0}">
-	    			<i class='icon-shopping_cart' :class="{heighlight: totalCount >0}"></i>
-	  			</div>
-	  			<div class="num" v-show='totalCount>0'>{{totalCount}}</div>
-	  		</div>
-	  		<div class="price" :class="{heighlight: totalCount >0}">￥{{totalPrice}}</div>
-	  		<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
-    	</div>
-    	<div class="content-right">
-	    	<div class="pay" :class='payClass'>
-	    		{{payDesc}}  
-	    	</div>
-	    </div>
-    </div>
+	<div class="shopcart">
+		<div class="content">
+			<div class="content-left">
+				<div class="logo-wrapper">
+					<div class="logo" :class="{heighlight: totalCount >0}">
+						<i class='icon-shopping_cart' :class="{heighlight: totalCount >0}"></i>
+					</div>
+					<div class="num" v-show='totalCount>0'>{{totalCount}}</div>
+				</div>
+				<div class="price" :class="{heighlight: totalCount >0}">￥{{totalPrice}}</div>
+				<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+			</div>
+			<div class="content-right">
+				<div class="pay" :class='payClass'>
+					{{payDesc}}
+				</div>
+			</div>
+		</div>
 		<div class="ball-container">
-				<transition-group 
-				  v-on:before-enter="beforeEnter"
-					v-on:enter="enter"
-					v-on:after-enter="afterEnter"
-				>
-					<div v-for='ball,idx in balls' v-show='ball.show' class='ball' key='idx'>
+			<!-- 数据结构的锅  -->
+			<div v-for="ball in balls">
+				<transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+					<div class="ball" v-show="ball.show">
 						<div class="inner inner-hook"></div>
 					</div>
-				</transition-group>
+				</transition>
+			</div>
 		</div>
-  </div>
+	</div>
 </template>
 <script>
 export default {
-  name: 'shopcart',
-  props: {
-  	selectFoods: {
+	name: 'shopcart',
+	props: {
+		selectFoods: {
 			type: Array,
-			default () {
+			default() {
 				return [
 					{
-					price: 19,
-					count: 1
-				}]
+						price: 19,
+						count: 1
+					}]
 			}
-  	},
-  	deliveryPrice: {
-  		type: Number,
-  		default: 0
-  	},
-  	minPrice: {
-  		type: Number,
-  		default: 0
-  	}
-  },
-	created () {
+		},
+		deliveryPrice: {
+			type: Number,
+			default: 0
+		},
+		minPrice: {
+			type: Number,
+			default: 0
+		}
+	},
+	created() {
 		console.log(this.selectFoods)
 	},
-	data () {
+	data() {
 		return {
 			balls: [
 				{
@@ -78,228 +77,215 @@ export default {
 			dropBalls: []
 		}
 	},
-  computed: {
-  	totalPrice () {
-  		let total = 0
-  		this.selectFoods.forEach((food) => {
-  			total += food.price * food.count
-  		})
-  		return total
-  	},
+	computed: {
+		totalPrice() {
+			let total = 0
+			this.selectFoods.forEach((food) => {
+				total += food.price * food.count
+			})
+			return total
+		},
 
-  	totalCount () {
-  		let count = 0
-  		this.selectFoods.forEach((food) => {
-  			count += food.count
-  		})
-  		return count
-  	},
-  	payDesc () {
-  		if (this.tatalPrice === 0) {
-  			return `￥${minPrice}元起送`
-  		}else if (this.totalPrice < this.minPrice) {
-  			let diff = this.minPrice - this.totalPrice
-  			return `还差￥${diff}元起送`
-  		}else {
-  			return  '去结算'
-  		}
-  	},
-  	payClass () {
-  		if (this.totalPrice < this.minPrice) {
-  			return 'not-enough'
-  		}else{
-  			return 'enough'
-  		}
-  	}
-  },
+		totalCount() {
+			let count = 0
+			this.selectFoods.forEach((food) => {
+				count += food.count
+			})
+			return count
+		},
+		payDesc() {
+			if (this.tatalPrice === 0) {
+				return `￥${minPrice}元起送`
+			} else if (this.totalPrice < this.minPrice) {
+				let diff = this.minPrice - this.totalPrice
+				return `还差￥${diff}元起送`
+			} else {
+				return '去结算'
+			}
+		},
+		payClass() {
+			if (this.totalPrice < this.minPrice) {
+				return 'not-enough'
+			} else {
+				return 'enough'
+			}
+		}
+	},
 	methods: {
-		drop (el) {
-			// console.log(el)
-			for (let i= 0; i< this.balls.length; i++) {
-				let ball = this.balls[i]
-				console.log(ball)
-				if(!ball.show) {
-					ball.show = true
-					ball.el = el
-					this.dropBalls.push(ball)
-
-					console.log(this.dropBalls[0])
+		drop(el) {
+			for (let [idx, item] of this.balls.entries()) {
+				if (!item.show) {
+					item.show = true
+					item.el = el
+					this.dropBalls.push(item)
 					return
 				}
 			}
 		},
 
-		beforeEnter (el) {
-			console.log('before')
-			console.log(el)
+		// 过渡.. 还是参考了源码- -.
+		beforeDrop(el) {
 			let count = this.balls.length
-			console.log(count + 'ball.length')
-			while(count--) {
+			while (count--) {
 				let ball = this.balls[count]
-				if(ball.show) {
-					let rect = ball.el.getBoundingClientRect()
-					let x = rect.left - 32
-					let y = -(window.innerHeight - rect.top - 22)
-					el.style.display = ''
-					el.style.transform = `translate3d(0, ${y}px, 0)`
-					let inner = el.getElementsByClassName('inner-hook')[0]
-					inner.style.transform = `translate3d(${x}px, 0, 0)`
-					console.log('wancheng')
+				if (ball.show) {
+					let rect = ball.el.getBoundingClientRect();
+					let x = rect.left - 32;
+					let y = -(window.innerHeight - rect.top - 22);
+					el.style.display = '';
+					el.style.webkitTransform = `translate3d(0,${y}px,0)`;
+					el.style.transform = `translate3d(0,${y}px,0)`;
+					let inner = el.getElementsByClassName('inner-hook')[0];
+					inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+					inner.style.transform = `translate3d(${x}px,0,0)`;
 				}
 			}
 		},
 
-		enter (el, done) {
+		dropping(el, done) {
 			/* eslint-disable no-unused-vars */
 			// let rf = el.offsetHeight
 			this.$nextTick(() => {
-				el.style.transform = 'translate3d(0, 0, 0)'
-				let inner = el.getElementsByClassName('inner-hook')[0]
-				inner.style.transform = 'translate3d(0, 0, 0)'
+				el.style.webkitTransform = 'translate3d(0,0,0)';
+				el.style.transform = 'translate3d(0,0,0)';
+				let inner = el.getElementsByClassName('inner-hook')[0];
+				inner.style.webkitTransform = 'translate3d(0,0,0)';
+				inner.style.transform = 'translate3d(0,0,0)';
+				// 过渡transition 结束后触发
+				el.addEventListener('transitionend', done);
 			})
-			let ball = this.dropBalls.shift()		
-			if(ball) {
-				console.log(ball)
-				ball.show = false
-				// el.style.display = 'none'
-			}
-			done()
 		},
 
-		// afterEnter(el) {
-		// 	console.log(this.dropBalls)
-
-		// 	let ball = this.dropBalls.shift()		
-		// 	if(ball) {
-		// 		console.log(ball)
-		// 		ball.show = false
-		// 		// el.style.display = 'none'
-		// 	}
-		// }
+		afterDrop(el) {
+			let ball = this.dropBalls.shift();
+			if (ball) {
+				console.log(el)
+				ball.show = false;
+				el.style.display = 'none';
+			}
+		}
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='less' rel="stylesheet/less" scpoed>
-	.shopcart {
-		position: fixed;
-		left: 0;
-		bottom: 0;
-		z-index: 50;
-		width: 100%;
-		height: 48px;
-		.content {
-			display: flex;	
-			background: #141d27;
-			color: rgba(255, 255, 255, 0.4);
-			font-size: 0;
-			.content-left {
-				flex: 1;
-				.logo-wrapper {
-					display: inline-block;
-					vetical-align: top;
-					position: relative;
-					top: -10px;
-					margin: 0 12px;
-					padding: 6px;
-					width: 56px;
-					height: 56px;
-					box-sizing: border-box;
+.shopcart {
+	position: fixed;
+	left: 0;
+	bottom: 0;
+	z-index: 50;
+	width: 100%;
+	height: 48px;
+	.content {
+		display: flex;
+		background: #141d27;
+		color: rgba(255, 255, 255, 0.4);
+		font-size: 0;
+		.content-left {
+			flex: 1;
+			.logo-wrapper {
+				display: inline-block;
+				vertical-align: top;
+				position: relative;
+				top: -10px;
+				margin: 0 12px;
+				padding: 6px;
+				width: 56px;
+				height: 56px;
+				box-sizing: border-box;
+				border-radius: 50%;
+				background: #141d27;
+				.logo {
+					height: 100%;
+					width: 100%;
 					border-radius: 50%;
-					background: #141d27;
-					.logo {
-						height: 100%;
-						width: 100%;
-						border-radius: 50%;
-						background: #2b343c;
-						text-align: center;
-						&.heighlight {
-							background: rgb(0, 160, 220);
-						}
-						.icon-shopping_cart {
-							font-size: 24px;
-							color: #80858a;
-							line-height: 44px;
-							&.heighlight {
-								color: #fff;
-							}
-						}
-					}
-					.num {
-						position: absolute;
-						top: 0;
-						right: 0;
-						width: 24px;
-						height: 16px;
-						line-height: 16px;
-						text-align: center;
-						border-radius: 16px;
-						font-size: 9px;
-						font-weight: 700;
-						color: #fff;
-						background: rgb(240, 20, 20);
-						box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
-					}
-				}
-				.price {
-					display: inline-block;
-					vertical-align: top;
-					line-height: 24px;
-					margin-top: 12px;
-					box-sizing: border-box;
-					padding-right: 12px;
-					border-right: 1px solid rgba(255, 255, 255, 0.1);
-					font-size: 16px;
-					font-weight: 700;
+					background: #2b343c;
+					text-align: center;
 					&.heighlight {
-						color: #fff;
+						background: rgb(0, 160, 220);
+					}
+					.icon-shopping_cart {
+						font-size: 24px;
+						color: #80858a;
+						line-height: 44px;
+						&.heighlight {
+							color: #fff;
+						}
 					}
 				}
-				.desc {
-					display: inline-block;
-					vertical-align: top;
-					line-height: 24px;
-					margin: 12px 0 0 12px;
-					font-size: 10px;
+				.num {
+					position: absolute;
+					top: 0;
+					right: 0;
+					width: 24px;
+					height: 16px;
+					line-height: 16px;
+					text-align: center;
+					border-radius: 16px;
+					font-size: 9px;
+					font-weight: 700;
+					color: #fff;
+					background: rgb(240, 20, 20);
+					box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
 				}
 			}
-			.content-right {
-				flex: 0 0 105px;
-				width: 105px;
-				.pay {
-					height: 48px;
-					line-height: 48px;
-					text-align: center;
-					font-size: 12px;
-					font-weight:700;
-					&.not-enough {
-						background: #2b333b;
-					}
-					&.enough {
-						background: #00b43c;
-						color: #fff;
-					}
+			.price {
+				display: inline-block;
+				vertical-align: top;
+				line-height: 24px;
+				margin-top: 12px;
+				box-sizing: border-box;
+				padding-right: 12px;
+				border-right: 1px solid rgba(255, 255, 255, 0.1);
+				font-size: 16px;
+				font-weight: 700;
+				&.heighlight {
+					color: #fff;
 				}
+			}
+			.desc {
+				display: inline-block;
+				vertical-align: top;
+				line-height: 24px;
+				margin: 12px 0 0 12px;
+				font-size: 10px;
 			}
 		}
-		.ball-container {
-			.ball {
-				position: fixed;
-				left: 32px;
-				bottom: 22px;
-				z-index: 500;
-				transition: all .4s cubic-bezier(0.49, -0.29, 0.75, 0.41);
-				.inner {
-					width: 16px;
-					height: 16px;
-					border-radius: 50%;
-					background: rgb(0 ,160, 220);
-					transition: all .4s linear;
+		.content-right {
+			flex: 0 0 105px;
+			width: 105px;
+			.pay {
+				height: 48px;
+				line-height: 48px;
+				text-align: center;
+				font-size: 12px;
+				font-weight: 700;
+				&.not-enough {
+					background: #2b333b;
+				}
+				&.enough {
+					background: #00b43c;
+					color: #fff;
 				}
 			}
 		}
 	}
-
-
+	.ball-container {
+		.ball {
+			position: fixed;
+			left: 32px;
+			bottom: 22px;
+			z-index: 500;
+			transition: all .4s cubic-bezier(0.49, -0.29, 0.75, 0.41);
+			.inner {
+				width: 16px;
+				height: 16px;
+				border-radius: 50%;
+				background: rgb(0, 160, 220);
+				transition: all .4s linear;
+			}
+		}
+	}
+}
 </style>
