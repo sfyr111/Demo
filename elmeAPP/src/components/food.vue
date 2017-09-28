9358ac397aa29c1e5a37251167eb6d20d5e616f1
张久屹 <447488560@qq.com>
2017-09-29 00:06:43 +0800

    elem

--- elmeAPP/src/components/food.vue
+++ elmeAPP/src/components/food.vue
@@ -18,25 +18,52 @@
             <span class='now'>￥{{food.price}}</span>
             <span class='old' v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
           </div>
+
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food='food' @addCart="addCart"></cartcontrol>
+          </div>
+          <div class="buy" v-show='!food.count || food.count===0' @click='addFirst'>
+            加入购物车
+          </div>
         </div>
 
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food='food' @addCart="addCart"></cartcontrol>
+        <split v-show='food.info' />
+
+        <div class="info" v-show='food.info'>
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <div class="buy" v-show='!food.count || food.count===0' @click='addFirst'>加入购物车</div>
+
+        <split/>
+
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :selectType='selectType' :onlyContent='onlyContent' :desc='desc' :ratings='food.ratings' />
+        </div>
+
       </div>
     </div>
   </transition>
 </template>
 
 <script>
+
+
 import BScroll from 'better-scroll'
 import cartcontrol from './cartcontrol'
+import split from './split'
+import ratingselect from './ratingselect'
 import Vue from 'vue'
+
+const POSITIVE = 0
+const NEGATIVE = 1
+const ALL = 2
 
 export default {
   components: {
-    cartcontrol
+    cartcontrol,
+    ratingselect,
+    split
   },
   props: {
     food: {
@@ -45,12 +72,22 @@ export default {
   },
   data() {
     return {
-      showFlag: false
+      showFlag: false,
+      selectType: ALL,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽',
+      }
     }
   },
   methods: {
     show() {
       this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
+
       this.$nextTick(() => {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.elFood, {
@@ -124,6 +161,7 @@ export default {
     }
   }
   .content {
+    position: relative;
     padding: 18px;
     .title {
       line-height: 14px;
@@ -160,33 +198,58 @@ export default {
         color: rbb(147, 153, 159);
       }
     }
+    .cartcontrol-wrapper {
+      position: absolute;
+      bottom: 12px;
+      right: 12px;
+    }
+    .buy {
+      position: absolute;
+      right: 18px;
+      bottom: 18px;
+      z-index: 10;
+      height: 24px;
+      line-height: 24px;
+      padding: 0 12px;
+      box-sizing: border-box;
+      font-size: 10px;
+      border-radius: 12px;
+      color: #fff;
+      background-color: rgb(0, 160, 220)
+    }
+    &.fade-enter-active,
+    &.fade-leave-active {
+      transition: 1s;
+    }
+    &.fade-enter,
+    &.fade-leave-to {
+      opacity: 0;
+    }
   }
-  .cartcontrol-wrapper {
-    position: absolute;
-    bottom: 12px;
-    right: 12px;
+  .info {
+    padding: 18px;
+    .title {
+      line-height: 14px;
+      margin-bottom: 6px;
+      font-size: 14px;
+      color: rgb(7, 17, 27);
+    }
+    .text {
+      line-height: 24px;
+      padding: 0 8px;
+      font-size: 12px;
+      color: rgb(77, 85, 93);
+    }
   }
-  .buy {
-    position: absolute;
-    right: 18px;
-    bottom: 18px;
-    z-index: 10;
-    height: 24px;
-    line-height: 24px;
-    padding: 0 12px;
-    box-sizing: border-box;
-    font-size: 10px;
-    border-radius: 12px;
-    color: #fff;
-    background-color: rgb(0, 160, 220)
-  }
-  &.fade-enter-active,
-  &.fade-leave-active {
-    transition: 1s;
-  }
-  &.fade-enter,
-  &.fade-leave-to {
-    opacity: 0;
+  .rating {
+    padding-top: 18px;
+    .title {
+      line-height: 14px;
+      margin-bottom: 6px;
+      margin-left: 18px;
+      font-size: 14px;
+      color: rgb(7, 17, 27);
+    }
   }
 }
 </style>
---
+++ elmeAPP/src/components/ratingselect.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="ratingselect">
+    <div class="rating-type border-1px">
+      <span class='block  positive'>{{desc.all}}
+        <span class='count'>
+          </47span>
+        </span>
+        <span class='block  positive'>{{desc.positive}}
+          <span class='count'>40</span>
+        </span>
+        <span class='block  negative'>{{desc.negative}}
+          <span class='count'>7</span>
+        </span>
+    </div>
+    <div class="switch">
+      <span class="icon-check_circle"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+const POSITIVE = 0
+const NEGATIVE = 1
+const ALL = 2
+
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    selectType: {
+      type: Number,
+      default: ALL
+    },
+    onlyContent: {
+      type: Boolean,
+      default: false
+    },
+
+    desc: {
+      type: Object,
+      default() {
+        return {
+          all: '全部',
+          positive: '满意',
+          negative: '不满意'
+        }
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang='less' rel="stylesheet/less" scpoed>
+@import '../common/stylus/mixin.less';
+
+.ratingselect {
+  .rating-type {
+    padding: 18px 0;
+    margin: 0 18px;
+    .border-1px(rgba(7, 17, 27, 0.1));
+    font-size: 0;
+  }
+  .block {
+    display: inline-block;
+    padding: 8px 12px;
+    margin-right: 8px;
+    border-radius: 1px;
+    color: rgb(77, 85, 93); // color: #fff;
+    &.positive {
+      color: rgba(77, 85, 93);
+      background: rgba(0, 160, 0, 2);
+    }
+    &.negative {
+      background: rgba(0, 160, 0, 2);
+    }
+  }
+}
+</style>
+
+
