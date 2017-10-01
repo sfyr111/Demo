604268f67948aad73d5181bfe621940be9dd238f
张久屹 <447488560@qq.com>
2017-10-01 23:30:55 +0800

    fix

--- elmeAPP/src/components/split.vue
+++ elmeAPP/src/components/split.vue
@@ -1,25 +1,21 @@
 <template>
-  <div class="split" >
-
-
+  <div class="split">
   </div>
 </template>
 
 
 <script>
 export default {
-  
+
 }
 </script>
 
-
 <style lang='less' rel="stylesheet/less" scpoed>
-.split{
+.split {
   height: 16px;
-  border-top: 1px solid rgba(7,17,27,0.1);
-  border-bottom: 1px solid rgba(7,17,27,0.1);
+  border-top: 1px solid rgba(7, 17, 27, 0.1);
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
   background: #f3f5f7;
 }
-
 </style>
 
