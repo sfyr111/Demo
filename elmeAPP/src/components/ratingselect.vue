8e9e4cea64545ddc2cda7ae7379d0a9d043c0ac3
张久屹 <447488560@qq.com>
2017-10-02 02:44:44 +0800

    ratingselect coponent optimize

--- elmeAPP/src/components/ratingselect.vue
+++ elmeAPP/src/components/ratingselect.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span class='block  positive'>{{desc.all}}
+      <span class='block positive' :class='{active:selectType===2}'>{{desc.all}}
         <span class='count'>47</span>
       </span>
-      <span class='block positive'>{{desc.positive}}
+      <span class='block positive' :class='{active:selectType===0}'>{{desc.positive}}
         <span class='count'>40</span>
       </span>
-      <span class='block negative'>{{desc.negative}}
+      <span class='block negative' :class='{active:selectType===1}'>{{desc.negative}}
         <span class='count'>7</span>
       </span>
     </div>
-    <div class="switch">
-      <span class="icon-check_circle">sd</span>
+    <div class="switch" :class='{on:onlyContent}'>
+      <span class="icon-check_circle"> </span>
       <span class="text">只看有内容的评价</span>
     </div>
   </div>
@@ -65,19 +65,57 @@ export default {
     margin: 0 18px;
     .border-1px(rgba(7, 17, 27, 0.1));
     font-size: 0;
-  }
-  .block {
-    display: inline-block;
-    padding: 8px 12px;
-    margin-right: 8px;
-    border-radius: 1px;
-    color: rgb(77, 85, 93);
-    &.positive {
-      // color: rgba(77, 85, 93);
-      background: rgba(0, 160, 0, 2);
+    .block {
+      display: inline-block;
+      font-size: 12px;
+      padding: 8px 12px;
+      line-height: 16px;
+      margin-right: 8px;
+      border-radius: 1px;
+      color: rgb(77, 85, 93);
+      &.active {
+        color: #fff;
+      }
+      .count {
+        font-size: 8px;
+        margin-left: 2px;
+        line-height: 16px;
+      }
+      &.positive {
+        background: rgba(0, 160, 220, 0.2);
+        &.active {
+          background: rgb(0, 160, 220);
+        }
+      }
+      &.negative {
+        background: rgba(77, 85, 93, 0.2);
+        &.active {
+          background: rgb(77, 85, 93);
+        }
+      }
     }
-    &.negative {
-      background: rgba(0, 160, 0, 2);
+  }
+  .switch {
+    padding: 12px 18px;
+    line-height: 24px;
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    color: rgb(147, 153, 159);
+    &.on {
+      .icon-check_circle {
+        color: #00c850;
+      }
+    }
+    .icon-check_circle {
+      display: inline-block;
+      vertical-align: top;
+      border: 1px solid red;
+      margin-right: 4px;
+      font-size: 24px;
+    }
+    .text {
+      display: inline-block;
+      vertical-align: top;
+      font-size: 12px;
     }
   }
 }
